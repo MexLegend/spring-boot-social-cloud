d7997fc1fed112d395d6574e432671f6c30ddbb4
MexLegend <armandoskate2011@hotmail.com>
2023-11-17 14:17:42 -0600

    Initial commit

---
+++ src/test/java/com/vsionary/springsocialcloud/SpringSocialCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vsionary.springsocialcloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSocialCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
